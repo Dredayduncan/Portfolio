914daa53a5e9155e952fda326f88a6e1537804b3
Andrew Duncan <dredayduncan@gmail.com>
2022-11-28 17:13:52 +0000

    replaced bootstrap carousel with swiperJS to fix the issue

--- src/views/Portfolio.js
+++ src/views/Portfolio.js
@@ -7,6 +7,14 @@ import HireButton from "../components/HireButton";
 import mySafeCampus from "../assets/images/mySafeCampus.png";
 import MobileAppView from '../components/mobileAppView';
 import WebAppView from '../components/webAppView';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y, } from 'swiper';
+// Import Swiper styles
+import 'swiper/swiper.min.css';
+import 'swiper/modules/pagination/pagination.min.css';
+import 'swiper/modules/scrollbar/scrollbar.min.css';
+import 'swiper/modules/navigation/navigation.min.css';
+
 
 function Portfolio(){
     
@@ -19,7 +27,7 @@ function Portfolio(){
                 <h1 data-scroll data-scroll-speed="1" data-scroll-class="appear" data-scroll-repeat="true" className='pt-4'>Some of my projects</h1>
             </div>
 
-            <div id="carouselExampleIndicators" className="pt-2 carousel slide" data-bs-ride="carousel" data-scroll data-scroll-speed="1" data-scroll-class="appear" data-scroll-repeat="true">
+            {/* <div id="carouselExampleIndicators" className="pt-2 carousel slide" data-bs-ride="carousel" data-scroll data-scroll-speed="1" data-scroll-class="appear" data-scroll-repeat="true">
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -28,25 +36,10 @@ function Portfolio(){
                 </div>
                 <div className="carousel-inner rounded">
                     <div className="carousel-item active" style={{"background": "#1b542f"}} data-bs-interval="2000">
-                        <WebAppView 
-                            image={spearCapital} 
-                            name="Spear Capital & Advisory" 
-                            description=" A website for Spear Capital Advisory Limited; a Ghanaian company that aids other enterprises to upscale their businesses. " 
-                            color= "#ffffff"
-                            link="https://www.spearcapitaladvisory.com"
-
-                        />
+                        
                     </div>
                     <div className="carousel-item" style={{"background": "#8f2e2e"}} data-bs-interval="2000">
-                        <MobileAppView 
-                            image={mySafeCampus} 
-                            name="My Safe Campus" 
-                            description=" My Safe Campus is a system that aims to allow members of a local university to report cases of sexual misconduct easily 
-                            and train them on the university's sexual misconduct policies. The training system and the incident reporting system are two of its components. 
-                            The reporting system which is the mobile application enables users to report and raise alarms of incidences of sexual misconduct, 
-                            while the training system, the web application, instructs and tests the users on appropriate sexual misbehavior through lessons and quizzes.
-                            " 
-                        />
+                       
                     </div>
                     <div className="carousel-item" style={{"background": "#969796"}} data-bs-interval="2000">
                         <WebAppView 
@@ -61,19 +54,75 @@ function Portfolio(){
                     </div>
                     
                     <div className="carousel-item" style={{"background": "#ac3e46"}} data-bs-interval="2000">
-                        <MobileAppView 
-                            image={ashesiCom} 
-                            name="AshesiCom" 
-                            description=" A twitter clone for the community of a local university. It allows people belonging to this community to share their thoughts, 
-                            current status and updates with the rest of the community through posts. Users of this application can react through likes, 
-                            comments and reposts with these posts to create a sense of community through the interaction." 
-                            link="https://github.com/Dredayduncan/AshesiComAppWithFlutter"
-                        />
+                       
                     </div>
                 </div>
-            </div>
+            </div> */}
 
         </div>
+
+        <div className='container' data-scroll data-scroll-speed="1" data-scroll-class="appear" data-scroll-repeat="true">
+            <Swiper
+            // install Swiper modules
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            // spaceBetween={50}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <WebAppView 
+                        image={asvie} 
+                        name="Asvie" 
+                        description=" Asvie, a virtual integrated voice assistant, is based on Edward Jarvis' famous Artificial Intelligence Assistant, Jarvis, from the Marvel universe. 
+                        The main goal of this project is to create a Jarvis that is compatible with Ashesi students. It is a tool for keeping students up to date on events, academic assignments, 
+                        and aid with research. With verbal cues in the form of requests, students are able to retrieve their grades, upcoming assignments, lectures with the times and locations they occur at, 
+                        read their emails and make internet searches." 
+                    />
+                </SwiperSlide>
+                
+                <SwiperSlide>
+                    <MobileAppView 
+                        image={mySafeCampus} 
+                        name="My Safe Campus" 
+                        description=" My Safe Campus is a system that aims to allow members of a local university to report cases of sexual misconduct easily 
+                        and train them on the university's sexual misconduct policies. The training system and the incident reporting system are two of its components. 
+                        The reporting system which is the mobile application enables users to report and raise alarms of incidences of sexual misconduct, 
+                        while the training system, the web application, instructs and tests the users on appropriate sexual misbehavior through lessons and quizzes.
+                        " 
+                    />
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <WebAppView 
+                        image={spearCapital} 
+                        name="Spear Capital & Advisory" 
+                        description=" A website for Spear Capital Advisory Limited; a Ghanaian company that aids other enterprises to upscale their businesses. " 
+                        color= "#ffffff"
+                        link="https://www.spearcapitaladvisory.com"
+
+                    />
+                </SwiperSlide>
+                
+                <SwiperSlide>
+                    <MobileAppView 
+                        image={ashesiCom} 
+                        name="AshesiCom" 
+                        description=" A twitter clone for the community of a local university. It allows people belonging to this community to share their thoughts, 
+                        current status and updates with the rest of the community through posts. Users of this application can react through likes, 
+                        comments and reposts with these posts to create a sense of community through the interaction." 
+                        link="https://github.com/Dredayduncan/AshesiComAppWithFlutter"
+                    />
+                </SwiperSlide>
+        
+            </Swiper>
+
+        </div>
+
+        
                
         
           
